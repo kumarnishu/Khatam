7778c68ccbae60d59531edbf337c979dba8339fe
nishu kumar <kumarnishu437@gmail.com>
2023-12-15 04:19:37 +0530

    reverted

--- utils/CreateWhatsappClient.ts
+++ utils/CreateWhatsappClient.ts
@@ -1,6 +1,5 @@
-import makeWASocket, { makeInMemoryStore, useMultiFileAuthState } from '@whiskeysockets/baileys'
+import makeWASocket, { makeInMemoryStore, proto, useMultiFileAuthState } from '@whiskeysockets/baileys'
 import qrcode from "qrcode-terminal"
-import fs from "fs"
 
 
 const store = makeInMemoryStore({})
@@ -22,9 +21,7 @@ async function CreateSocket(session_folder: string) {
 export async function HandleSocket(session_id: string) {
     const { socket, saveCreds } = await CreateSocket(session_id)
     socket.ev.on('connection.update', async (conn) => {
-        console.log(conn)
         if (conn.connection === "close") {
-          
             HandleSocket(session_id)
         }
         if (conn.connection === "connecting") {
@@ -36,16 +33,78 @@ export async function HandleSocket(session_id: string) {
             })
     })
     socket.ev.on('creds.update', saveCreds)
-    socket.ev.on('messages.upsert', (data) => {
+    socket.ev.on('messages.upsert', async (data) => {
         console.log(data.messages)
         data.messages.forEach(async (msg) => {
             let id = msg.key.remoteJid
+            if (id)
+                await socket.sendPresenceUpdate('composing', id)
             if (msg && id && !msg.key.fromMe) {
-                await socket.sendMessage(id, {
-                    video: { url: "https://www.w3schools.com/tags/movie.mp4" }
-                })
+                await HandleMessage(id, socket, msg)
             }
         })
-        socket.logout()
     })
+}
+
+async function HandleMessage(id: string, socket: any, message: proto.IWebMessageInfo) {
+
+    // const id = 'abcd@s.whatsapp.net' // the WhatsApp ID 
+    // send a simple text!
+
+    // send a reply messagge
+    await socket.sendMessage(id, { text: 'oh hello there' }, { quoted: message })
+    // send a mentions message
+    await socket.sendMessage(id, { text: '@12345678901', mentions: ['12345678901@s.whatsapp.net'] })
+    // send a location!
+    await socket.sendMessage(
+        id,
+        { location: { degreesLatitude: 24.121231, degreesLongitude: 55.1121221 } }
+    )
+    // send a contact!
+    const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+        + 'VERSION:3.0\n'
+        + 'FN:Jeff Singh\n' // full name
+        + 'ORG:Ashoka Uni;\n' // the organization of the contact
+        + 'TEL;type=CELL;type=VOICE;waid=911234567890:+91 12345 67890\n' // WhatsApp ID + phone number
+        + 'END:VCARD'
+    await socket.sendMessage(
+        id,
+        {
+            contacts: {
+                displayName: 'Jeff',
+                contacts: [{ vcard }]
+            }
+        }
+    )
+
+
+    const reactionMessage = {
+        react: {
+            text: "💖", // use an empty string to remove the reaction
+            key: message.key
+        }
+    }
+    // send a link
+    await socket.sendMessage(id, { text: 'Hi, this was sent using https://github.com/adiwajshing/baileys' })
+    await socket.sendMessage(
+        id,
+        {
+            video: { url: "https://www.w3schools.com/tags/movie.mp4" }, mimetype: 'video/mp4',
+            caption: "hello!"
+        }
+    )
+    await socket.sendMessage(
+        id,
+        {
+            document: { url: "https://ik.imagekit.io/ghzlr9kj8/Agarson%20Catlouge.pdf?updatedAt=1699345149889" }, mimetype: "*",
+            caption: "hello! pdf"
+        }
+    )
+
+    // send an audio file
+    await socket.sendMessage(
+        id,
+        { audio: { url: "https://www.w3schools.com/html/horse.ogg" }, mimetype: 'audio/mp4' },
+    )
+    await socket.sendMessage(id, reactionMessage)
 }
